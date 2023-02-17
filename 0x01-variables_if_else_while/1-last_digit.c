9d0acf0a646157492335d2b730e9e4be23c6c6ac
codegode23 <jasperakabua020@gmail.com>
2023-02-17 15:26:43 -0800

    Committing Task 1. The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+# include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10; /*The last digit is stored in the last_digit variable*/
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	else
+	{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	}
+
+	return (0);
+
+}
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,7 @@
 # Tasks and what problems they solve
 
 ## Task 0. Positive anything is better than negative nothing - This program will assign a random number to the variable n each time it is executed. It will print whether the number stored in the variable n is positive or negative.
+
+## Task 1. The last digit - This program will assign a random number to the variable n each time it is executed. Here the last digit is determined.
+
+##
